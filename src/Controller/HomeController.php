c2898aeb26e85654ead3b28db6943f1e4a5e0537
timfel20 <timmyfelicia@gmail.com>
2022-09-23 21:41:16 +0200

    homecontroller, product entity, fake data generator-productfixture generated and index data updated

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $productRepository->findAll(),
         ]);
     }
 }
---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+/**
+ * Class ProductFixtures
+ */
+class ProductFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $product = new Product();
+            $product
+                ->setName('Product ' . $i)
+                ->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
+                ->setPrice(mt_rand(10, 600));
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
