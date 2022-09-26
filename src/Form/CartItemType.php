cc58c1a5f6625d6e747736025757bf0a39e620ed
timfel20 <timmyfelicia@gmail.com>
2022-09-26 13:20:55 +0200

    cart contrller and view, user-cart session save and necessary forms for cart page built

--- src/Form/CartItemType.php
+++ src/Form/CartItemType.php
@@ -6,6 +6,7 @@ use App\Entity\OrderItem;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CartItemType extends AbstractType
 {
